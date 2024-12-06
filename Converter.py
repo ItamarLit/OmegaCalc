522a04033b63256af9fbf719216d2b2e7b775d9e
ItamarLit <itamarlitvin@gmail.com>
2024-12-06 13:04:21 +0200

    Fixed paren bugs

--- Converter.py
+++ Converter.py
@@ -1,5 +1,5 @@
 from ErrorHandler import ErrorHandler
-from Operators import Operator, IUnaryOperator, UMinus
+from Operators import Operator, IUnaryOperator, IBinaryOperator, UMinus
 from Errors import ConversionError
 
 
@@ -10,6 +10,7 @@ class Converter:
     1. Invalid paren ie missing ( to a ) token or missing ) to a (
     2. Invalid use of the ~ op ie it is not in front of a binary minus or a number
     3. Invalid unary minus use, ie it isn't before (, a number or another unary minus to the right
+    4. Invalid use of binary op after ( ie 1(+2
     The output list will be a list of tokens in postfix so that I can give better errors in the evaluator (get the position of the error)
     """
 
@@ -32,7 +33,7 @@ class Converter:
                         ConversionError("Invalid use of ~ op at pos: " + str(token.get_token_pos()[0])))
             else:
                 # the token is paren
-                self._handle_paren(token)
+                self._handle_paren(token_list, cur_index)
             cur_index += 1
         # call the end of input func
         self._handle_end_input()
@@ -46,8 +47,12 @@ class Converter:
         :param token_list:
         :return:
         """
-        # the tilda op can only come before a unary minus or a number
-        if token_list[cur_index + 1].get_token_type() != "Number" and not isinstance(token_list[cur_index + 1].get_token_value(), UMinus):
+        next_token = token_list[cur_index + 1] if cur_index + 1 < len(token_list) else None
+        prev_token = token_list[cur_index - 1] if cur_index - 1 >= 0 else None
+        # the tilda op can only come before a unary minus or a number and before the tilda there was nothing or a binary op
+        if next_token.get_token_type() != "Number" and not isinstance(next_token.get_token_value(), UMinus):
+            return False
+        if prev_token is not None and not isinstance(prev_token.get_token_value(), IBinaryOperator):
             return False
         return True
 
@@ -59,14 +64,20 @@ class Converter:
         """
         self._output_lst.append(token)
 
-    def _handle_paren(self, token):
+    def _handle_paren(self, token_list, cur_index):
         """
         Handles the paren
-        :param token:
+        :param token_list:
+        :param cur_index:
         :return:
         """
-        if token.get_token_value() == '(':
-            self._op_stack.append(token)
+        # add invalid paren errors if there are any
+        self._check_invalid_paren(token_list, cur_index)
+
+        cur_token = token_list[cur_index]
+        if cur_token.get_token_value() == '(':
+            # check for binary op
+            self._op_stack.append(cur_token)
         else:
             # paren is )
             if self._check_list_for_paren('('):
@@ -76,7 +87,61 @@ class Converter:
                 self._op_stack.pop()
             else:
                 self._error_handler.add_error(
-                    ConversionError("Missing Opening parentheses to opening parentheses at position: " + str(token.get_token_pos()[0])))
+                    ConversionError("Missing Opening parentheses to closing parentheses at position: " + str(
+                        cur_token.get_token_pos()[0])))
+
+    def _check_invalid_paren(self, token_list, cur_index):
+        """
+        This func is used to check for any errors in the expression that have to do with paren
+        :param token_list:
+        :param cur_index:
+        :return:
+        """
+        cur_paren = token_list[cur_index].get_token_type()
+        next_token = token_list[cur_index + 1] if cur_index + 1 < len(token_list) else None
+        prev_token = token_list[cur_index - 1] if cur_index - 1 >= 0 else None
+
+        # opening paren, check if next token is a binary op or !
+        if cur_paren == '(':
+            # ( can never finish an exp so i dont need to check if cur_index + 1 is none
+            next_token_type = next_token.get_token_type()
+            next_token_value = next_token.get_token_value()
+
+            # check that the token before the paren is not a number or !
+            if prev_token is not None and (
+                    prev_token.get_token_type() == '!' or prev_token.get_token_type() == "Number"):
+                self._error_handler.add_error(ConversionError(
+                    f"Invalid token before opening parentheses at position: {token_list[cur_index].get_token_pos()[0]}"))
+
+            if isinstance(next_token_value, IBinaryOperator) or next_token_type == '!':
+                self._error_handler.add_error(ConversionError(
+                    f"Invalid use of {next_token_type} at position:"
+                    f" {token_list[cur_index + 1].get_token_pos()[0]} cannot come after ("))
+                return
+
+            elif next_token_type == ')':
+                closing_paren_index = token_list[cur_index + 1].get_token_pos()[0]
+                self._error_handler.add_error(ConversionError(
+                    f"Invalid empty parentheses at position: {closing_paren_index - 1} -> {closing_paren_index}"))
+                return
+
+        # closing paren
+        else:
+            prev_token_type = token_list[cur_index - 1].get_token_type()
+            prev_token_value = token_list[cur_index - 1].get_token_value()
+            if isinstance(prev_token_value, IBinaryOperator) or (
+                    prev_token_type != '!' and isinstance(prev_token_value, IUnaryOperator)):
+                self._error_handler.add_error(ConversionError(
+                    f"Invalid use of {prev_token_type} at position:"
+                    f" {token_list[cur_index - 1].get_token_pos()[0]} cannot come before )"))
+                return
+            # check that the token before the paren is not a number or !
+            elif next_token is not None and (next_token.get_token_type() == "U-"
+                                             or next_token.get_token_type() == "Number"):
+                print(next_token.get_token_type())
+                self._error_handler.add_error(ConversionError(
+                    f"Invalid token after closing parentheses at position: {token_list[cur_index].get_token_pos()[0]}"))
+                return
 
     def _check_list_for_paren(self, paren_type: chr) -> bool:
         """
@@ -117,8 +182,9 @@ class Converter:
 
             if next_token.get_token_type() != "Number" and next_token.get_token_type() != "(" and next_token.get_token_type() != "U-":
                 # add the error to the error handler
-                self._error_handler.add_error(ConversionError(f"Invalid use of unary minus operator at position: {token_list[cur_index].get_token_pos()[0]}"
-                                                              f" cannot come before: {next_token.get_token_type()}"))
+                self._error_handler.add_error(ConversionError(
+                    f"Invalid use of unary minus operator at position: {token_list[cur_index].get_token_pos()[0]}"
+                    f" cannot come before: {next_token.get_token_type()}"))
 
     def _handle_end_input(self):
         """
@@ -127,11 +193,13 @@ class Converter:
         """
         if self._check_list_for_paren('('):
             # invalid exp missing ) to opening paren, get the indexes
-            invalid_indexes = list(filter(lambda pos: self._op_stack[pos].get_token_value() == '(', range(len(self._op_stack))))
+            invalid_indexes = list(
+                filter(lambda pos: self._op_stack[pos].get_token_value() == '(', range(len(self._op_stack))))
             for index in invalid_indexes:
                 # add the error
                 self._error_handler.add_error(
-                    ConversionError("Missing Closing parentheses to opening parentheses at position: " + str(self._op_stack[index].get_token_pos()[0])))
+                    ConversionError("Missing Closing parentheses to opening parentheses at position: " + str(
+                        self._op_stack[index].get_token_pos()[0])))
         else:
             while len(self._op_stack) != 0:
                 self._output_lst.append(self._op_stack.pop())
