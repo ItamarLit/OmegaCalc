9569613b5d4c5cc3c4af29d7e37aae916177c709
ItamarLit <itamarlitvin@gmail.com>
2024-12-12 20:08:27 +0200

    Fixed tests to handle error type

--- Tests/Base_Tests.py
+++ Tests/Base_Tests.py
@@ -6,43 +6,43 @@ Simple syntax tests along with 1 gibberish 1 empty 1 whitespace test
 def test_1(calc_handler):
     result, error_list = calc_handler.run_single_exp("2*^3")
     assert result is None
-    assert error_list[0].get_msg() == "Missing operands for: * at position: 1"
+    assert error_list[0].get_error_type() == "Missing_Operands_Error"
 
 
 def test_2(calc_handler):
     result, error_list = calc_handler.run_single_exp("2*")
     assert result is None
-    assert error_list[0].get_msg() == "Invalid Operator usage: * ,at position: 1"
+    assert error_list[0].get_error_type() == "Missing_Operands_Error"
 
 
 def test_3(calc_handler):
     result, error_list = calc_handler.run_single_exp("(+)")
     assert result is None
-    assert error_list[0].get_msg() == "Missing operands for: + at position: 1"
+    assert error_list[0].get_error_type() == "Missing_Operands_Error"
 
 
 def test_4(calc_handler):
     result, error_list = calc_handler.run_single_exp("(2+2")
     assert result is None
-    assert error_list[0].get_msg() == "Missing Closing parentheses to opening parentheses at position: 0"
+    assert error_list[0].get_error_type() == "Missing_Close_Paren_Error"
 
 
 def test_5(calc_handler):
     result, error_list = calc_handler.run_single_exp("~(2*3)")
     assert result is None
-    assert error_list[0].get_msg() == "Invalid use of ~ at position: 0"
+    assert error_list[0].get_error_type() == "Invalid_Unary_Usage_Error"
 
 
 def test_6(calc_handler):
     result, error_list = calc_handler.run_single_exp("asdasdasdasgLDLA")
     assert result is None
-    assert error_list[0].get_msg() == "Invalid Chars found: asdasdasdasgLDLA ,at position: 0 -> 15"
+    assert error_list[0].get_error_type() == "Invalid_Chars_Error"
 
 
 def test_7(calc_handler):
     result, error_list = calc_handler.run_single_exp("")
     assert result is None
-    assert error_list[0].get_msg() == "Invalid Input, The input must contain an expression"
+    assert error_list[0].get_error_type() == "Empty_Input_Error"
 
 
 def test_8(calc_handler):
